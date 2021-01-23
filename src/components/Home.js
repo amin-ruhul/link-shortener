cc5da6fbe953c76ccfaa16f320e8916e2af04691
amin_ruhul <ruhulinfo50@gmail.com>
2021-01-23 20:08:19 +0600

    get value

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Home() {
+  const [link, setLink] = useState("");
+
+  const handelSbmit = (e) => {
+    e.preventDefault();
+    console.log(link);
+  };
   return (
-    <div class="home">
+    <div className="home">
       <div className="input">
         <h2>Shot Link Generate</h2>
-        <form className="box">
-          <input type="text" placeholder="Enter you link to make is short" />
+        <form className="box" onSubmit={handelSbmit}>
+          <input
+            type="text"
+            placeholder="Enter you link to make it short"
+            value={link}
+            onChange={(e) => setLink(e.target.value)}
+          />
           <button className="btn" type="submit">
             Generate
           </button>
